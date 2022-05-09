b7ca46692396f39c1ab03fc46d8f1dcd42c1806b
jasanchez3 <jasanchez3@latam.stefanini.com>
2022-05-09 15:40:51 -0500

    🚧 [ADD] Background color

--- lib/src/pages/music_player_page/music_player_page.dart
+++ lib/src/pages/music_player_page/music_player_page.dart
@@ -8,8 +8,35 @@ class MusicPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: const [CustomAppBar(), CurrentSongInformation(), PlaySong(), LyricsList()],
+        body: Stack(
+      children: [
+        const _MusicPlayerBackground(),
+        Column(
+          children: const [CustomAppBar(), CurrentSongInformation(), PlaySong(), LyricsList()],
+        ),
+      ],
     ));
+  }
+}
+
+class _MusicPlayerBackground extends StatelessWidget {
+  const _MusicPlayerBackground({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      width: double.infinity,
+      height: screenSize.height * 0.75, // 75% of the screen height
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60)),
+          gradient: LinearGradient(begin: Alignment.centerLeft, end: Alignment.center, colors: [
+            Color(0xff33333E),
+            Color(0xff201E28),
+          ])),
+    );
   }
 }
